18da0100ecc5076ee96904fc389feb3e28c86d96
Luiz Otávio <luizomf@gmail.com>
2022-05-28 10:56:01 -0300

    GoTop

--- src/components/GoTop/index.jsx
+++ src/components/GoTop/index.jsx
@@ -2,8 +2,18 @@ import * as Styled from './styles';
 import { KeyboardArrowUp } from '@styled-icons/material-outlined/KeyboardArrowUp';
 
 export const GoTop = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <Styled.Container href="#" aria-label="Go to top" title="Go to top">
+    <Styled.Container
+      href="#"
+      onClick={handleClick}
+      aria-label="Go to top"
+      title="Go to top"
+    >
       <KeyboardArrowUp />
     </Styled.Container>
   );
