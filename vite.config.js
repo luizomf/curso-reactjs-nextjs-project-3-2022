9c2c45bc46b788c8bb88f01085dc11213129df9d
Luiz Otávio <luizomf@gmail.com>
2022-05-30 10:39:55 -0300

    trying new converage config

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,8 @@ export default defineConfig({
     setupFiles: ['../.test/setup.js'],
     include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'templates'],
+    // coverage: {
+    //   reporter: ['clover', 'json', 'lcov'],
+    // },
   },
 });
